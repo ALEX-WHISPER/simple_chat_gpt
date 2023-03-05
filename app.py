7b50a88859eb56f3956256b799a6d3d7ec739b68
ALEX-WHISPER <15196608305@163.com>
2023-03-06 01:39:39 +0800

    优化上下文功能, 之前是把所有历史记录累积式地发送, 导致对话越往后堆积的数据量越大; 现引入记忆窗口模式, 每次发送指令时, 只一并发送最近的指定数量的历史记录, 相当于每次的发送量是固定的, 也能维持一段可调节的上下文功能

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 import openai
 import gradio as gr
 import configobj
+import queue
+import math
 
 OPEN_AI_KEY_NAME = "OPENAI_API_KEY"  # 环境变量名, 需要将 openai 平台提供的 key 添加到系统的环境变量中
 personality_des = "你是一个热心友好的助手"  # 性格设定
-dialogue_records = []  # 历史记录, 用于支持上下文对话
-max_round = 10  # 历史记录的最大值, 限制 token 的消耗
-enable_context_support = False  # 是否开启上下文支持
+dialogue_records = queue.Queue()  # 历史记录, 用于支持上下文对话, 以队列形式组织, 队内元素超出上限则出队
+dialogue_memory_size = 5  # 历史记录窗口的最大值
+enable_context_support = True  # 是否开启上下文支持
 temperature_value = 0.6
 
 
@@ -18,7 +20,7 @@ def get_openai_response(input_msg):
     # system 设定在最前, 其次是历史记录, 最后是当前的用户输入
     message_list = [system_message]
     if enable_context_support:
-        for dialogue in dialogue_records:
+        for dialogue in dialogue_records.queue:
             message_list.append(dialogue)
     message_list.append(user_message)
 
@@ -38,12 +40,16 @@ def conversation_history(input_msg, history):
     history = history or []
     output_msg = get_openai_response(input_msg)
 
-    # 使对话支持上下文, 但 token 的消耗量也会随之叠加, 因此设置一个回合上限, 对话数量超出上限则清空历史记录
-    if len(dialogue_records) * 2 > max_round:
-        dialogue_records.clear()
+    # 当存储的对话记录数量超出窗口长度, 触发出队(连续2次, 包括指令与回答)
+    if math.ceil(dialogue_records.qsize() / 2) > dialogue_memory_size:
+        print("record size bigger than the maximum, pop the queue twice")
+        dialogue_records.get()
+        dialogue_records.get()
     else:
-        dialogue_records.append({"role": "user", "content": input_msg})
-        dialogue_records.append({"role": "assistant", "content": output_msg})
+        dialogue_records.put({"role": "user", "content": input_msg})
+        dialogue_records.put({"role": "assistant", "content": output_msg})
+
+    print("current record size: {}, max size: {}".format(dialogue_records.qsize() / 2, dialogue_memory_size))
 
     history.append((input_msg, output_msg))
 
@@ -56,6 +62,13 @@ def on_personality_changed(description):
     print("personality change to: {}".format(personality_des))
 
 
+def on_memory_size_changed(new_size):
+    global dialogue_memory_size
+    new_size = int(new_size)
+    dialogue_memory_size = max(2, min(new_size, 50))
+    print("memory size change to: {}".format(dialogue_memory_size))
+
+
 def on_temperature_changed(slider_num):
     global temperature_value
     temperature_value = min(max(0, slider_num), 1)
@@ -66,7 +79,7 @@ def on_context_switch_changed(enable):
     global enable_context_support
     enable_context_support = enable
     if not enable:
-        dialogue_records.clear()
+        dialogue_records.empty()
     return enable_context_support
 
 
@@ -83,14 +96,17 @@ if __name__ == "__main__":
         # 设置 api key
         openai.api_key = api_key_name
 
-        # 基于 gradio 设置 UI
+        # 基于 gradio 设置 UI, 以及各元素交互后的回调
         blocks = gr.Blocks()
         with blocks:
-            personality = gr.Textbox(label="personality", placeholder="Describe the way you want your assistant to act like", value=personality_des)
-            personality.change(on_personality_changed, inputs=[personality], outputs=[])
+            # personality = gr.Textbox(label="personality", placeholder="Describe the way you want your assistant to act like", value=personality_des)
+            # personality.change(on_personality_changed, inputs=[personality], outputs=[])
 
-            context_switch = gr.Checkbox(label="context switch", info="Enable context-based dialogue", value=enable_context_support)
-            context_switch.change(on_context_switch_changed, inputs=[context_switch], outputs=[])
+            # context_switch = gr.Checkbox(label="context switch", info="Enable context-based dialogue", value=enable_context_support)
+            # context_switch.change(on_context_switch_changed, inputs=[context_switch], outputs=[])
+
+            memory_size = gr.Number(label="memory size", value=dialogue_memory_size)
+            memory_size.change(on_memory_size_changed, inputs=[memory_size], outputs=[])
 
             temperature_slider = gr.Slider(0, 1, step=0.1, label="temperature")
             temperature_slider.change(on_temperature_changed, inputs=[temperature_slider], outputs=[])
